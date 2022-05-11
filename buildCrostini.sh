cbbda4b275a673fb812cf61382ca223ba2d592b9
Ken Robson <ken@robson.net>
2022-05-11 16:25:26 +0200

    Fixed typos

--- buildCrostini.sh
+++ buildCrostini.sh
@@ -31,8 +31,10 @@ lxc config set penguin security.nesting true
 # Create our build script locally in termina
 cat << EndOfBuildScript > /tmp/build.sh
 #!/bin/bash
+
 curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | /usr/bin/bash -lx
 exit
+
 # Install additonal packages using apt
 sudo apt update -y
 sudo apt upgrade -y
