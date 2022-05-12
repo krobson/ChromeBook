74a76477a0c844c826451551019f915199daee5a
Ken Robson <ken@robson.net>
2022-05-12 09:23:26 +0200

    Corrected config file backup

--- buildCrostini.sh
+++ buildCrostini.sh
@@ -155,8 +155,8 @@ git clone --bare git@github.com:krobson/myDotFiles.git $HOME/.cfg
 
 mkdir -p $HOME/.config-backup &&
   git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 |
-  egrep "\s+\." | awk {'print $1'} |
-  xargs -I{} bash -c "mkdir -p \$(dirname $HOME/{}); mv $HOME/{} $HOME/.config-backup/{}"
+  egrep "\s+\." | awk {'print $2'} |
+  xargs -I {} bash -c "mkdir -p $HOME/.cfg-backup/\$(dirname {}) && mv $HOME/{} $HOME/.cfg-backup/{}"
 
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 EndOfBuildScript
