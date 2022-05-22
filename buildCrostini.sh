915bdb6bd4ab720a56f9e27f766f492417aaa5d9
Ken Robson <ken@robson.net>
2022-05-22 08:17:03 +0200

    Update buildCrostini.sh

--- buildCrostini.sh
+++ buildCrostini.sh
@@ -65,6 +65,17 @@ sudo apt install -y \
   htop \
   libosinfo-bin \
   openjdk-17-jdk
+ 
+# Install vscode
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc -o microsoft.asc
+gpg --no-default-keyring --keyring ./ms_vscode_key_temp.gpg --import ./microsoft.asc
+gpg --no-default-keyring --keyring ./ms_vscode_key_temp.gpg --export > ./ms_vscode_key.gpg
+sudo mv ms_vscode_key.gpg /etc/apt/trusted.gpg.d/
+
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+sudo apt update
+sudo apt install code
 
 # Fix up NetworkManager dnsmasq configuation in preparation for crc install
 # Here documents need tabs not space for indents
@@ -87,7 +98,6 @@ flatpak install --assumeyes --noninteractive flathub \
   com.google.Chrome \
   org.mozilla.firefox \
   org.cryptomator.Cryptomator \
-  com.visualstudio.code \
   io.github.shiftey.Desktop \
   org.wireshark.Wireshark \
   com.basemark.BasemarkGPU \
