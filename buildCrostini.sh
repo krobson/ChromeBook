68162b247fa3d45e1f40cd9a73d1b592a5c0a324
Ken Robson <ken@robson.net>
2022-05-22 10:12:25 +0200

    Update buildCrostini.sh

--- buildCrostini.sh
+++ buildCrostini.sh
@@ -73,10 +73,11 @@ sudo apt install -y \
   bridge-utils
 
 # Install vscode
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc -o microsoft.asc
-gpg --no-default-keyring --keyring ./ms_vscode_key_temp.gpg --import ./microsoft.asc
-gpg --no-default-keyring --keyring ./ms_vscode_key_temp.gpg --export > ./ms_vscode_key.gpg
-sudo mv ms_vscode_key.gpg /etc/apt/trusted.gpg.d/
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc -o $HOME/microsoft.asc
+gpg --no-default-keyring --keyring $HOME/ms_vscode_key_temp.gpg --import $HOME/microsoft.asc
+gpg --no-default-keyring --keyring $HOME/ms_vscode_key_temp.gpg --export > $HOME/ms_vscode_key.gpg
+sudo mv $HOME/ms_vscode_key.gpg /etc/apt/trusted.gpg.d/
+rm $HOME/microsoft.asc $HOME/ms_vscode_key_temp.gpg
 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
@@ -99,6 +100,8 @@ sudo systemctl restart NetworkManager
 
 # Install Software TPM
 sudo cat <<- 'EndOfTpmFile' > /etc/apt/sources.list.d/swtpm.list
+	deb [trusted=yes] http://ppa.launchpad.net/stefanberger/swtpm-focal/ubuntu focal main
+EndOfTpmFile
 
 sudo apt update
 sudo apt install swtpm-tools -y
